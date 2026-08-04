f394418ae042bf36a08e144fad918bd557bbd8e3
kubok758 <artemborovitskiy0312@gmail.com>
2026-08-04 12:02:32 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,26 @@
-{
-  "compilerOptions": {
-    "composite": true,
-    "skipLibCheck": true,
-    "module": "ESNext",
-    "moduleResolution": "Bundler",
-    "allowImportingTsExtensions": true,
-    "noEmit": true
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  // Название репозитория GitHub обязательно указывается здесь.
+  base: '/Galaxy-OneUI-Simulator/',
+
+  plugins: [react()],
+
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
   },
-  "include": [
-    "vite.config.ts",
-    "playwright.config.ts"
-  ]
-}
+
+  preview: {
+    host: '0.0.0.0',
+    port: 4173,
+  },
+
+  test: {
+    environment: 'jsdom',
+    setupFiles: ['./src/test/setup.ts'],
+    css: true,
+    globals: true,
+  },
+});
